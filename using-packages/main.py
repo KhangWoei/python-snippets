b076fa82557dc48c48d48bcfb3eeefca123d26b8
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-09-22 23:39:32 +0100

    testing doc attrib

--- using-packages/main.py
+++ using-packages/main.py
@@ -1,12 +1,19 @@
 def main(input: str):
+    """
+    Hi from main's doc string
+    """
     print("Hi, {0}".format(input))
 
 if __name__ == "__main__":
     import sys
     import simple_package
     import simple_package.submodule
+
     input = str(sys.argv[1])
+
     main(input)
+    print(main.__doc__)
+
     simple_package.main(input)
     simple_package.submodule.main(input)
 

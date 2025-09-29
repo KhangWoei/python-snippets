6b4bc59aaa74c60cce2cfb8841f5bb2c651815a8
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-09-29 23:48:26 +0100

    messing around with hatch

---
+++ python-concepts/build-systems/src/metadata.py
@@ -0,0 +1,19 @@
+import json 
+
+from hatchling.metadata.plugin.interface import MetadataHookInterface
+
+
+class JSONMetaDataHook(MetadataHookInterface):
+    def update(self, metadata):
+        with open("metadata.json") as src:
+            constants = json.load(src)
+
+            metadata["version"] = constants["version"]
+            metadata["authors"] = [
+                {
+                    "name": author["name"],
+                    "email": author["email"]
+                } for author in constants["authors"]
+            ]
+            metadata["license"] = constants["license"]
+

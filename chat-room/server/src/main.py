df311478e64d80cc052a7b30d63a61f8bae3c342
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-10-08 23:21:54 +0100

    refactoring

--- chat-room/server/src/main.py
+++ chat-room/server/src/main.py
@@ -4,46 +4,34 @@ import signal
 import sys
 
 class Server():
+
     def __init__(self, address: str, port: int):
-        self._address = address
-        self._port = port
+        self._socket = socket(AF_INET, SOCK_STREAM)
+        self._socket.bind((address, port))
 
-    def start_server(self) -> socket:
-        server = socket(AF_INET, SOCK_STREAM)
-        server.bind((self._address, self._port))
+
+    def start_server(self) -> None:
+        server = self._socket
+
         server.listen(5)
+        print(f"Listening on {server.getsockname()}")
 
-        return server
+        client, addr = server.accept()
+        
+        try:
+            while True:
+                request = client.recv(1024)
+                print(f"Received: {request}")
 
+                client.send("Ping received".encode())
+        finally:
+            server.close()
+            sys.exit(0)
 
 if __name__ == "__main__":
     config = cfg.load()
     print(f"{config.address}:{config.port}")
 
-    server = Server(config.address, config.port).start_server()
+    server = Server(config.address, config.port)
 
-    print(f"Listening on {server.getsockname()}")
-    
-    def cleanup(sig, frame):
-        print("shutting down..")
-        server.shutdown(SHUT_RDWR)
-        server.close()
-        sys.exit(0)
-
-    signal.signal(signal.SIGTERM, cleanup)
-    signal.signal(signal.SIGTERM, cleanup)
-
-    client, addr = server.accept()
-    print(f"Accepted connection from {addr}")
-    
-    try:
-        while True:
-            request = client.recv(1024)
-            print(f"Received: {request}")
-
-            client.send("Ping received".encode())
-    except ConnectionResetError:
-        print("client shutdown received")
-    finally
-        server.close()
-        sys.exit(0)
+    server.start_server()

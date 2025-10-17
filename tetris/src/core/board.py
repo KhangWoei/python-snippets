70c74514fbb9384c08a975dc626e9d66cd2cd898
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-10-17 20:27:41 +0100

    curses doesn't like drawing to the bottom right cell

--- tetris/src/core/board.py
+++ tetris/src/core/board.py
@@ -6,20 +6,33 @@ class Board():
     _board_width = 10
 
     def __init__(self, window: window, block_size: BlockSize) -> None:
+        self._block_size = block_size
         max_height, max_width = window.getmaxyx()
         
         mapped_height = self._board_height * block_size.height
         mapped_width = self._board_width * block_size.width
-        
-        start_y = (max_height - (mapped_height)) // 2
-        start_x = (max_width - (max_width)) // 2
 
-        self._window = newwin(mapped_height, mapped_width, start_y, start_x)
-        pass
-    
+        start_y = (max_height - (mapped_height)) // 2
+        start_x = (max_width - (mapped_width)) // 2
+        
+        self._window = newwin(mapped_height + 1, mapped_width, start_y, start_x)
+        self._board = [[1 for _ in range(self._board_width)] for _ in range(self._board_height)]
+
     @property
     def window(self) -> window:
         return self._window
 
     def render(self) -> None:
-        pass
+        self._render_cells()
+        self._window.refresh()
+
+    def _render_cells(self) -> None:
+        for row_index, row in enumerate(self._board):
+            for col_index, cell in enumerate(row):
+                if cell != 0:
+                    cell_y = row_index * self._block_size.height
+                    cell_x = col_index * self._block_size.width
+
+                    for height in range(self._block_size.height):
+                        self.window.addstr(cell_y + height, cell_x, "[" + "x" * (self._block_size.width - 2) + "]")
+

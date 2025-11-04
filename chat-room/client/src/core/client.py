66d8b9e7ce9564fba038b5e818be3059b3dcfa1d
Khang <41323253+KhangWoei@users.noreply.github.com>
2025-11-04 16:17:20 +0000

    error handling and cleanup

--- chat-room/client/src/core/client.py
+++ chat-room/client/src/core/client.py
@@ -1,22 +1,29 @@
 from socket import socket, AF_INET, SOCK_STREAM, SHUT_RDWR
-from threading import Thread
-from types import TracebackType
-from typing import Self
+from threading import Thread, Event
 
 class Client():
 
     def __init__(self) -> None:
         self._socket: socket = socket(AF_INET, SOCK_STREAM)
-        self._running: bool = False
+        self._running: Event = Event()
 
     def connect(self, address: str, port: int) -> None:
-        self._socket.connect((address, port))
-        self._running = True
+        try:
+            self._socket.connect((address, port))
+            self._running.set()
 
-        receiver = Thread(target = self._receive_loop, daemon=True)
-        receiver.start()
-
-        self._send_loop()
+            receiver = Thread(target = self._receive_loop, daemon=True)
+            receiver.start()
+            
+            self._send_loop()
+        except KeyboardInterrupt:
+            print("Disconnecting")
+        except ConnectionRefusedError:
+            print(f"Failed to connect to {address}:{port}")
+        except OSError:
+            pass
+        finally:
+            self.disconnect()
 
     def _receive_loop(self) -> None:
         while self._running:
@@ -29,14 +36,16 @@ class Client():
                 self.disconnect()
  
     def _send_loop(self) -> None:
-        try:
-            while self._running:
-                message: str = input("> ")
-                self._socket.send((message).encode())
-        except (KeyboardInterrupt):
-            self.disconnect()
+        while self._running:
+            message: str = input("> ")
+            self._socket.send((message).encode())
 
     def disconnect(self) -> None:
-        self._running = False
-        self._socket.shutdown(SHUT_RDWR)
-        self._socket.close()
+        try:
+            self._running.clear()
+            self._socket.shutdown(SHUT_RDWR)
+        except OSError:
+            pass
+        finally:
+            self._socket.close()
+
